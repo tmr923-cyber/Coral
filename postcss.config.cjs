df3a5c949a8a5b20dd16c57086ddb459d6d36176
tmr923-cyber <saahir923@gmail.com>
2025-10-20 16:03:30 +0000

    3

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,7 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'postcss-nesting': {},          // choose one nesting plugin only
-    '@tailwindcss/postcss': {},     // new Tailwind PostCSS plugin (required for Tailwind v4+)
-    autoprefixer: {}
+    tailwindcss: {},
+    autoprefixer: {},
   }
 };
 
